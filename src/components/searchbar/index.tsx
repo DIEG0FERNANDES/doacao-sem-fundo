ec947bf9667cbc96153159651ac10e39b3074a02
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-12-13 01:07:28 -0400

    Update Index.tsx

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Button } from 'react-native';
 import { Bar, Container } from './styles';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar: React.FC = () => {
   return (
     <Container>
       <Bar>
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
       </Bar>
       <Button title="Filtrar" onPress={() => {}} />
     </Container>
